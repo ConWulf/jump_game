75a471d36eff05444504a6539acffdb3e1ee3751
Connor Wulf <connor.m.wulf@gmail.com>
2021-06-01 14:30:25 -0500

    character can jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,15 @@ public class PlayerController : MonoBehaviour
 
     private PlayerActionControls pac;
     [SerializeField] private float speed, jumpSpeed;
+    [SerializeField] private LayerMask ground;
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    
 
     private void Awake() {
         pac = new PlayerActionControls();
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
     }
 
     private void OnEnable() {
@@ -22,7 +28,26 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        pac.Land.Jump.performed += _ => Jump();
+    }
+
+    private void Jump() {
+        if(IsGrounded()) {
+            Physics.gravity.y * 2;
+            rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded() {
+        Vector2 topLeftPoint = transform.position;
+        topLeftPoint.x -= coll.bounds.extents.x;
+        topLeftPoint.y += coll.bounds.extents.y;
+
+        Vector2 bottomRightPoint = transform.position;
+        bottomRightPoint.x += coll.bounds.extents.x;
+        bottomRightPoint.y -= coll.bounds.extents.y;
+
+        return Physics2D.OverlapArea(topLeftPoint, bottomRightPoint, ground);
     }
 
     // Update is called once per frame
